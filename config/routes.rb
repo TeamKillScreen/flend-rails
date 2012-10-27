4dad7817c92cef4edc29d3d7cd62f9545925573b
Richard Pearson <github@catdevnull.co.uk>
2012-10-27 19:05:33 +0100

    Static Pages controller, about & contact

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Flend::Application.routes.draw do
 
   resources :items
 
-  match "/" => "items#index"
+  root to: 'static_pages#home'
+
+  match '/about',      to: 'static_pages#about'
+  match '/contact',    to: 'static_pages#contact'
 
 end
