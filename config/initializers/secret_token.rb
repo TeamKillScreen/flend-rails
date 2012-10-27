a7ad11ed0390bfdfd4e2e67306a1d6ed7dc44739
Richard Pearson <github@catdevnull.co.uk>
2012-10-27 17:05:27 +0100

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flend::Application.config.secret_token = 'be997932a1b97f96ad434a26fc1ce2c5a82f5f95269d841c4208a6ba83f098ad4c6039fea52521774240bf163cc518c3942bb6c39bd703c722be7deadcb33324'
