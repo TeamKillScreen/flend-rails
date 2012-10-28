187c212075b7c9ef9bba54297942fe8c2768c7cd
Richard Pearson <github@catdevnull.co.uk>
2012-10-28 04:29:12 +0000

    Can now click through from items on dashboard map

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,6 +14,7 @@ class StaticPagesController < ApplicationController
   		redirect_to sign_in_path
   	else
   		@items = Item.all
+  		@user = session[:user]
 
   		@json = @items.to_gmaps4rails
   	end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveResource::Base
 
   self.site = "http://flendapi.azurewebsites.net"
   self.element_name = "items"
+
+  def gmaps4rails_infowindow
+  	"<h4><a href='/items/#{self.id}'>#{self.title}</a></h4><p>#{self.description}</p>"
+  end
+
+  def gmaps4rails_title
+  	self.title
+  end
 end
